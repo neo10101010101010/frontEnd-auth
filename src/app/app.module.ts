de686f71e451c7fb578a39b386a76fc9ab95774e
Neo Neo <jesusggxbk@gmail.com>
2023-07-21 15:56:47 -0600

    “primero”

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'src/header/header.component';
+import { AuthContentComponent } from './auth-content/auth-content.component';
+import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthContentComponent,
+    WelcomeContentComponent,
+    LoginFormComponent,
+    ContentComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth-content/auth-content.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AxiosService } from '../axios.service';
+
+@Component({
+  selector: 'app-auth-content',
+  templateUrl: './auth-content.component.html',
+  styleUrls: ['./auth-content.component.css']
+})
+export class AuthContentComponent {
+  data: string[] = [];
+
+  constructor(private axiosService: AxiosService){
+    
+  }
+
+  ngOnInit(): void{
+    this.axiosService.request(
+      "GET",
+      "/messages",
+      {}
+    ).then(
+      (Response) => this.data = Response.data
+    )
+  }
+}
